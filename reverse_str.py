60dd5e685ec9e49005cdc1903193c877eaf0e2c4
jddunn <johnnyddunn@gmail.com>
2018-03-04 08:36:43 -0500

    add reverse str implementation with stack

--- reverse_str.py
+++ reverse_str.py
@@ -1,27 +1,69 @@
 # Reverses a string
 
+class Stack():
+
+    def __init__(self, items):
+        self.items = items
+
+    def __str__(self):
+    	return(''.join(self.items))
+
+    def size(self):
+        return len(self.items)
+
+    def is_empty(self):
+        return(self.items == [])
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def pop(self):
+        top = self.items[len(self.items)-1]
+        self.items = self.items[:-1]
+        return top
+
+    def push(self, val):
+        self.items.append(val)
+        return self.items
+
+
 class ReverseStr():
 
-	def reverse(self, str_text):
-		temp_str = [0] * len(str_text)
-		last_pos = len(str_text) - 1
+	def reverse_with_list(self, text):
+		temp_str = [0] * len(text)
+		last_pos = len(text) - 1
 		pos = 0
-		for each in str_text:
-			temp_str[pos] = str_text[last_pos]
+		for each in text:
+			temp_str[pos] = text[last_pos]
 			pos += 1
 			last_pos -= 1
-		self.str_text = ''.join(temp_str)
-		return self.str_text
+		self.text = ''.join(temp_str)
+		return self.text
+
+	def reverse_with_stack(self, text):
+		char_stack = Stack([])
+		for each in text:
+			char_stack.push(each)
+		reverse_stack = Stack([])
+		for each in text:
+			reverse_stack.push(char_stack.pop())
+		return str(reverse_stack)
 
 
 import unittest
 
 class ReverseStrTest(unittest.TestCase):
 
-	def test_reverse(self):
+	def test_reverse_with_stack(self):
 		reverse_str = ReverseStr()
-		print("Testing string reversed: ", "hey there", " - ", "ereht yeh")
-		self.assertEqual(reverse_str.reverse("hey there"), "ereht yeh")
+		print("Testing string reversed (with stack): ", "hey there", " - ", "ereht yeh")
+		self.assertEqual(reverse_str.reverse_with_stack("hey there"), "ereht yeh")
+
+	def test_reverse_with_list(self):
+		reverse_str = ReverseStr()
+		print("Testing string reversed (with list): ", "hey there", " - ", "ereht yeh")
+		self.assertEqual(reverse_str.reverse_with_list("hey there"), "ereht yeh")
+
 
 if __name__ == '__main__':
 	unittest.main()
